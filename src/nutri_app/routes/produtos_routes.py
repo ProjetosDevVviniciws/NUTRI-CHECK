ef1948a3f19aa455c259f3ccd9c7ee08bac35451
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-07-01 22:22:50 -0300

    Adiciona rota '/produtos' para cadastro de produtos

---
+++ src/nutri_app/routes/produtos_routes.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, render_template, redirect, url_for, flash
+from src.nutri_app.forms.produtos_forms import ProdutoForm
+from src.nutri_app.database import engine
+from sqlalchemy import text
+from flask_login import login_required
+
+produtos_bp = Blueprint('produtos', __name__)
+
+@produtos_bp.route('/produtos', methods=['GET', 'POST'])
+@login_required
+def cadastrar_produto():
+    forms = ProdutoForm()
+    if forms.validate_on_submit():
+        nome = forms.nome.data
+        codigo_barras = forms.codigo_barras.data
+        porcao = float(forms.porcao.data)
+        calorias = float(forms.porcao.data)
+        proteinas = float(forms.proteinas.data)
+        carboidratos = float(forms.carboidratos.data)
+        gorduras = float(forms.gorduras.data)
+        
+        with engine.connect() as conn:
+            query = text("""
+                INSERT INTO produtos (nome, codigo_barras, porcao, calorias, proteinas, carboidratos, gorduras)
+                VALUES (:nome, :codigo_barras, :porcao, :calorias, :proteinas, :carboidratos, :gorduras)
+                """)
+            conn.execute(query, {
+                "nome": nome,
+                "codigo_barras": codigo_barras,
+                "porcao": porcao,
+                "calorias": calorias,
+                "proteinas": proteinas,
+                "carboidratos": carboidratos,
+                "gorduras": gorduras
+            })
+            flash("Produto adicionado com sucesso!", category="success")
+            return redirect(url_for('refeicao.registrar_refeicao'))
+    if forms.errors != {}:
+        for err in forms.errors.values(): 
+            flash(f"Erro ao cadastrar produto: {err}", category="danger")
+            return render_template("produtos.html", form=forms)
+       
