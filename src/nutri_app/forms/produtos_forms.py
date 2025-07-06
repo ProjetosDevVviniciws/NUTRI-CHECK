f429b04796558f45733de5a463b207a6789a107a
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-07-05 23:32:46 -0300

    Atualiza a classe ProdutoForm, adicionando labels em todos os campos

--- src/nutri_app/forms/produtos_forms.py
+++ src/nutri_app/forms/produtos_forms.py
@@ -3,11 +3,11 @@ from wtforms.validators import DataRequired, Optional, Length
 from flask_wtf import FlaskForm
 
 class ProdutoForm(FlaskForm):
-    nome = StringField('Nome:' , validators=[DataRequired(), Length(min=2, max=100)])
-    codigo_barras = StringField('Código de Barras:', validators=[Optional(), Length(max=50)])
-    porcao = FloatField('Porção (g):', validators=[DataRequired()])
-    calorias = FloatField('Calorias:', validators=[DataRequired()])
-    proteinas = FloatField('Proteínas:', validators=[DataRequired()])
-    carboidratos = FloatField('Carboidratos:', validators=[DataRequired()])
-    gorduras = FloatField('Gorduras:', validators=[DataRequired()])
-    submit = SubmitField('Adicionar Produto')
+    nome = StringField(label='Nome:' , validators=[DataRequired(), Length(min=2, max=100)])
+    codigo_barras = StringField(label='Código de Barras:', validators=[Optional(), Length(max=50)])
+    porcao = FloatField(label='Porção (g):', validators=[DataRequired()])
+    calorias = FloatField(label='Calorias:', validators=[DataRequired()])
+    proteinas = FloatField(label='Proteínas:', validators=[DataRequired()])
+    carboidratos = FloatField(label='Carboidratos:', validators=[DataRequired()])
+    gorduras = FloatField(label='Gorduras:', validators=[DataRequired()])
+    submit = SubmitField(label='Adicionar Produto')
