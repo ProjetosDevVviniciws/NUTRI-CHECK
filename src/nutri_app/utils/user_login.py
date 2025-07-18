d608e9ea35f098e28fccf778db2c3f3ede8b0d6f
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-07-18 19:52:25 -0300

    Implementa função load_user usada pelo login_manager para carregar usuário autenticado

--- src/nutri_app/utils/user_login.py
+++ src/nutri_app/utils/user_login.py
@@ -1,4 +1,7 @@
 from flask_login import UserMixin
+from src.nutri_app import login_manager
+from src.nutri_app.database import engine
+from sqlalchemy import text
 
 class UserLogin(UserMixin):
     def __init__(self, usuario_row):
@@ -9,3 +12,14 @@ class UserLogin(UserMixin):
         
     def get_id(self):
         return str(self.id)
+    
+@login_manager.user_loader
+def load_user(user_id):
+    with engine.connect() as conn:
+        query = text("SELECT * FROM usuarios WHERE id = :id")
+        result = conn.execute(query, {"id": int(user_id)})
+        row = result.fetchone()
+        
+        if row:
+            return UserLogin(row)
+        return None
