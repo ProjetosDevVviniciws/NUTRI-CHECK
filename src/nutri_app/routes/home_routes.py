6c694a96183bf0257efd5f6f5e8e67fbd458f2fa
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-07-20 22:56:03 -0300

    Implementa rota '/' que renderiza a página inicial do sistema

--- src/nutri_app/routes/home_routes.py
+++ src/nutri_app/routes/home_routes.py
@@ -1,30 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
-from src.nutri_app.database import engine
-from src.nutri_app.utils.hash import verificar_senha
-from src.nutri_app.utils.user_login import UserLogin
-from src.nutri_app.forms.auth_forms import LoginForm
-from sqlalchemy import text
-from flask_login import login_user
+from flask_login import login_required
 
 home_bp = Blueprint('home', __name__)
 
-@home_bp.route('/', methods=['GET', 'POST'])
+@home_bp.route('/')
 def home():
-    forms = LoginForm()
-    if forms.validate_on_submit():
-        usuario = forms.usuario.data
-        senha = forms.senha.data
-        
-        with engine.connect() as conn:
-            query = (text("SELECT * FROM usuarios WHERE usuario = :usuario"))
-            result = conn.execute(query, {"usuario": usuario}).fetchone()
-            
-        if result:
-            if verificar_senha(result.senha, senha):
-                user = UserLogin(result)
-                login_user(user)
-                flash(f"Sucesso! Bem-Vindo(a), {result.usuario}", category="success")
-                return redirect(url_for('produtos.cadastrar_produto'))
-            else:
-                flash("Usuário ou senha estão incorretos! Tente novamente.", category="danger")
-    return render_template("home.html", form=forms)
+    return render_template("home.html")
