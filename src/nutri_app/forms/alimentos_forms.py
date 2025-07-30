1a695e3a1f5a01d793853b8b885c73b9cae0beec
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-07-29 21:43:15 -0300

    Atualiza formulário adicionando campos calorias, proteínas, carboidratos, gorduras e atualiza label do submit

--- src/nutri_app/forms/alimentos_forms.py
+++ src/nutri_app/forms/alimentos_forms.py
@@ -1,4 +1,4 @@
-from wtforms import StringField, FloatField, SubmitField
+from wtforms import StringField, FloatField, SubmitField, DecimalField
 from wtforms.validators import DataRequired, Optional, Length
 from flask_wtf import FlaskForm
 
@@ -6,4 +6,8 @@ class AlimentoForm(FlaskForm):
     nome = StringField(label='Nome:' , validators=[DataRequired(), Length(min=2, max=100)])
     codigo_barras = StringField(label='Código de Barras:', validators=[Optional(), Length(max=50)])
     porcao = FloatField(label='Porção (g):', validators=[DataRequired()])
-    submit = SubmitField(label='Adicionar Alimento')
+    calorias = DecimalField("Calorias", validators=[Optional()])
+    proteinas = DecimalField("Proteínas", validators=[Optional()])
+    carboidratos = DecimalField("Carboidratos", validators=[Optional()])
+    gorduras = DecimalField("Gorduras", validators=[Optional()])
+    submit = SubmitField(label='Cadastrar Alimento')
