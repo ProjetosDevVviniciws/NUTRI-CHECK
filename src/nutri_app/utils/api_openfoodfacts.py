b62a5e7bbfa9c6d82ee4cce62928a42cf70e9910
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-08-03 22:27:16 -0300

    Adiciona função buscar_por_nome para consultar alimentos na API OpenFoodFacts

--- src/nutri_app/utils/api_openfoodfacts.py
+++ src/nutri_app/utils/api_openfoodfacts.py
@@ -24,3 +24,22 @@ def buscar_por_codigo_barras(codigo_barras):
         "carboidratos": nutriments.get("carbohydrates_100g", 0),
         "gorduras": nutriments.get("fat_100g", 0),
     }
+
+def buscar_por_nome(nome_alimento):
+    url = f"https://br.openfoodfacts.org/cgi/search.pl?search_terms={nome_alimento}&search_simple=1&action=process&json=1&page_size=1"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        resultados = response.json().get("products")
+        if resultados:
+            produto = resultados[0]
+            return {
+                "nome": produto.get("product_name", ""),
+                "calorias": produto.get("nutriments", {}).get("energy-kcal_100g"),
+                "proteinas": produto.get("nutriments", {}).get("proteins_100g"),
+                "carboidratos": produto.get("nutriments", {}).get("carbohydrates_100g"),
+                "gorduras": produto.get("nutriments", {}).get("fat_100g")
+            }
+    return None
+
+
