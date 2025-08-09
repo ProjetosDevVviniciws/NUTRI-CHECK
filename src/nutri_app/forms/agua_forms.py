b8d5cba0b4c8ce4cf39157bcdea27fe0f4378662
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-08-08 21:25:09 -0300

    Corrige indexação do atributo quantidade na classe AguaForm

--- src/nutri_app/forms/agua_forms.py
+++ src/nutri_app/forms/agua_forms.py
@@ -3,12 +3,9 @@ from wtforms import IntegerField, SubmitField
 from wtforms.validators import DataRequired, NumberRange
 
 class AguaForm(FlaskForm):
-    quantidade = IntegerField(
-        label="Quantidade de água (ml)",
+    quantidade = IntegerField(label="Quantidade de água (ml)",
         validators=[
             DataRequired(message="Informe a quantidade de água."),
-            NumberRange(min=50, max=12000, message="Informe entre 50ml e 12000ml.")
-        ]
-    )
+            NumberRange(min=50, max=12000, message="Informe entre 50ml e 12000ml.")])
     
     submit = SubmitField("Registrar Água")
