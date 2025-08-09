f1a96da33d7a02cce615e33c4b3220398cb41232
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-08-08 21:23:07 -0300

    Corrige indexação dos atributos da classe PerfilForm

--- src/nutri_app/forms/perfil_forms.py
+++ src/nutri_app/forms/perfil_forms.py
@@ -4,52 +4,35 @@ from wtforms.validators import DataRequired, Length, NumberRange, Optional
 
 class PerfilForm(FlaskForm):
     
-    sexo = SelectField(
-        label="Sexo",
+    sexo = SelectField(label="Sexo",
         choices=[
             ('masculino', 'Masculino'),
-            ('feminimo', 'Femimino')
-        ]
-    )
+            ('feminimo', 'Femimino')])
     
-    nome = StringField(
-        label="Nome",
+    nome = StringField(label="Nome",
         validators=[
             DataRequired(message="Informe seu nome."),
-            Length(min=2, max=60, message="Nome deve ser entre 2 e 50 caracteres.")
-        ]
-    )
+            Length(min=2, max=60, message="Nome deve ser entre 2 e 50 caracteres.")])
     
-    altura = DecimalField(
-        label="Altura (cm)",
+    altura = DecimalField(label="Altura (cm)",
         validators=[
             DataRequired(message="Informe sua altura em cm."),
-            NumberRange(min=50, max=250, message="Altura deve estar entre 50 e 250cm.")
-        ]
-    )
+            NumberRange(min=50, max=250, message="Altura deve estar entre 50 e 250cm.")])
     
-    peso = DecimalField(
-        label="Peso (kg)",
+    peso = DecimalField(label="Peso (kg)",
         validators=[
             DataRequired(message="Informe seu peso em kg."),
-            NumberRange(min=20, max=300, message="Peso deve estar entre 20 e 300 kg.")
-        ]
-    )
+            NumberRange(min=20, max=300, message="Peso deve estar entre 20 e 300 kg.")])
     
     idade = IntegerField(
         label="Idade",
         validators=[
             DataRequired(message="Informe sua idade."),
-            NumberRange(min=10, max=120, message="Idade deve estar entre 10 e 120 anos.")
-        ]
-    )
+            NumberRange(min=10, max=120, message="Idade deve estar entre 10 e 120 anos.")])
     
-    senha = PasswordField(
-        label="Nova Senha (opcional)",
+    senha = PasswordField(label="Nova Senha (opcional)",
         validators=[
             Optional(),
-            Length(min=6, message="A senha deve ter no mínimo 6 caracteres.")
-        ]
-    )
+            Length(min=6, message="A senha deve ter no mínimo 6 caracteres.")])
     
     submit = SubmitField(label="Salvar Alterações")
