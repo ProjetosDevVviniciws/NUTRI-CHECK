84f128396c1fcd6dc4725c14ca336c745ef7eb7b
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-09-02 11:26:21 -0300

    Atualiza rota / para resetar os valores de consumo diário do usuário quando a data nao coincide com a última atualizaçao

--- src/nutri_app/routes/home_routes.py
+++ src/nutri_app/routes/home_routes.py
@@ -12,12 +12,33 @@ def home():
     hoje = date.today()
     
     with engine.begin() as conn:
-        totais_dia = conn.execute(text("""
+        usuario = conn.execute(text("""
             SELECT calorias_consumidas, proteinas_consumidas, carboidratos_consumidos, gorduras_consumidas, ultima_atualizacao
             FROM usuarios
             WHERE id = :usuario_id AND ultima_atualizacao = :ultima_atualizacao
         """), {"usuario_id": current_user.id, "ultima_atualizacao": hoje}).mappings().first()
 
+        if not usuario or usuario["ultima_atualizacao"] != hoje:
+            conn.execute(text("""
+                UPDATE usuarios
+                SET calorias_consumidas = 0,
+                    proteinas_consumidas = 0,
+                    carboidratos_consumidos = 0,
+                    gorduras_consumidas = 0,
+                    ultima_atualizacao = :hoje
+                WHERE id = :usuario_id
+            """), {"hoje": hoje, "usuario_id": current_user.id})
+
+            totais_dia = {
+                "calorias_consumidas": 0,
+                "proteinas_consumidas": 0,
+                "carboidratos_consumidos": 0,
+                "gorduras_consumidas": 0,
+                "ultima_atualizacao": hoje
+            }
+        else:
+            totais_dia = usuario
+        
     return render_template(
         "pages/home.html",
         totais_dia=totais_dia,
