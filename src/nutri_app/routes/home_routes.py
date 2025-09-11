eced28e9675ebda9529703a999f4834491533a22
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-09-11 11:12:31 -0300

    Refatora rota / para tratar caso em que usuário não possui metas configuradas

--- src/nutri_app/routes/home_routes.py
+++ src/nutri_app/routes/home_routes.py
@@ -21,17 +21,7 @@ def home():
             WHERE id = :usuario_id AND ultima_atualizacao = :ultima_atualizacao
         """), {"usuario_id": current_user.id, "ultima_atualizacao": hoje}).mappings().first()
 
-        if not usuario or usuario["ultima_atualizacao"] != hoje:
-            conn.execute(text("""
-                UPDATE usuarios
-                SET calorias_consumidas = 0,
-                    proteinas_consumidas = 0,
-                    carboidratos_consumidos = 0,
-                    gorduras_consumidas = 0,
-                    ultima_atualizacao = :hoje
-                WHERE id = :usuario_id
-            """), {"hoje": hoje, "usuario_id": current_user.id})
-
+        if not usuario:
             totais_dia = {
                 "calorias_consumidas": 0,
                 "proteinas_consumidas": 0,
@@ -39,27 +29,17 @@ def home():
                 "gorduras_consumidas": 0,
                 "ultima_atualizacao": hoje
             }
-            
             metas_dia = {
-                "calorias_meta": usuario["calorias_meta"],
-                "proteinas_meta": usuario["proteinas_meta"],
-                "carboidratos_meta": usuario["carboidratos_meta"],
-                "gorduras_meta": usuario["gorduras_meta"]
+                "calorias_meta": 0,
+                "proteinas_meta": 0,
+                "carboidratos_meta": 0,
+                "gorduras_meta": 0
             }
-            
             restantes_dia = {
-                "calorias_restantes": usuario["calorias_restantes"],
-                "proteinas_restantes": usuario["proteinas_restantes"],
-                "carboidratos_restantes": usuario["carboidratos_restantes"],
-                "gorduras_restantes": usuario["gorduras_restantes"]
-            }
-        else:
-            totais_dia = {
-                "calorias_consumidas": usuario["calorias_consumidas"],
-                "proteinas_consumidas": usuario["proteinas_consumidas"],
-                "carboidratos_consumidos": usuario["carboidratos_consumidos"],
-                "gorduras_consumidas": usuario["gorduras_consumidas"],
-                "ultima_atualizacao": usuario["ultima_atualizacao"]
+                "calorias_restantes": 0,
+                "proteinas_restantes": 0,
+                "carboidratos_restantes": 0,
+                "gorduras_restantes": 0
             }
                   
     return render_template(
