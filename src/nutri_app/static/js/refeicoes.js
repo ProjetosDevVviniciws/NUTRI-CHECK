80849b1d5d4fc9cf0c7052b35cddfb331a322790
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-11-22 22:29:52 -0300

    Cria constante com ano, mes e dia mapeados a partir do value usando split('-')

--- src/nutri_app/static/js/refeicoes.js
+++ src/nutri_app/static/js/refeicoes.js
@@ -205,6 +205,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
 
         seletorData.addEventListener("change", (e) => {
+            const [ano, mes, dia] = e.target.value.split("-").map(Number);
             dataAtual = new Date(ano, mes - 1, dia);
             atualizarDataDisplay();
             carregarRefeicoes();
