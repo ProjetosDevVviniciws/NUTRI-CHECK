a74d99ab3758b8205055b9ee83171860641ae6c7
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-11-30 23:35:17 -0300

    Prepara a rota /agua para trabalhar com JSON adicionando import de 'jsonify'

--- src/nutri_app/routes/agua_routes.py
+++ src/nutri_app/routes/agua_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
 from src.nutri_app.forms.agua_forms import AguaForm
 from src.nutri_app.utils.decorators import perfil_completo_required
 from src.nutri_app.database import engine
