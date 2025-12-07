e7ec7d4f2d0947e32c102e0ed82ba3e8647ae7eb
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-12-06 23:52:39 -0300

    Remove rota /agua por não ser mais necessária

--- src/nutri_app/routes/agua_routes.py
+++ src/nutri_app/routes/agua_routes.py
@@ -7,12 +7,6 @@ from datetime import date
 
 agua_bp = Blueprint('agua', __name__)
 
-@agua_bp.route("/agua", methods=['GET'])
-@login_required
-@perfil_completo_required
-def pagina_agua():
-    return render_template("modals/registrar_agua_modal")
-
 @agua_bp.route("/agua/registrar", methods=['POST'])
 @login_required
 @perfil_completo_required
