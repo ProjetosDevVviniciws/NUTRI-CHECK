2be5a44cfe0ae3c9bcbd5a266f15da1d7d5edcde
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-12-09 23:25:12 -0300

    Adiciona condição para registro por data e corrige SQL na rota /agua/registrar

--- src/nutri_app/routes/agua_routes.py
+++ src/nutri_app/routes/agua_routes.py
@@ -21,6 +21,9 @@ def registrar_agua():
     except ValueError:
         return jsonify({"erro": "Quantidade inválida."}), 400
     
+    if quantidade < 50 or quantidade > 12000:
+        return jsonify({"erro": "Informe uma quantidade entre 50ml e 12000ml."}), 400
+    
     data_registro = data.get("data")
     if data_registro:
         try:
@@ -30,37 +33,30 @@ def registrar_agua():
     else:
         data_registro = date.today()
     
-    if quantidade < 50 or quantidade > 12000:
-        return jsonify({"erro": "Informe uma quantidade entre 50ml e 12000ml."}), 400
-    
-    hoje = date.today()
-    
     with engine.begin() as conn:
-        usuario = conn.execute(text("""
-            SELECT agua_consumida, ultima_atualizacao
-            FROM usuarios 
-            WHERE id = :id
-        """), {"id": current_user.id}).fetchone()
+        registro_agua = conn.execute(text("""
+            SELECT quantidade_ml 
+            FROM agua_registros
+            WHERE usuario_id = :id AND data = :data
+        """), {"id": current_user.id, "data": data_registro}).fetchone()
         
-        if usuario.ultima_atualizacao != hoje:
+        if registro_agua:
             conn.execute(text("""
-                UPDATE usuarios SET
-                    agua_consumida = 0,
-                    ultima_atualizacao = :hoje
-                WHERE id = :id
-            """), {"id": current_user.id, "hoje": hoje})
-            
-        conn.execute(text("""
-                UPDATE usuarios SET
-                    agua_consumida = agua_consumida + :qtd
-                WHERE id = :id
-            """), {"id": current_user.id, "qtd": quantidade})
+                UPDATE agua_registros 
+                SET quantidade_ml = quantidade_ml + :qtd
+                WHERE usuario_id = :id AND data = :data
+            """), {"qtd": quantidade,"id": current_user.id, "data": data_registro})
+        else:
+            conn.execute(text("""
+                INSERT INTO agua_registros (usuario_id, data, quantidade_ml)
+                VALUES (:id, :data, :qtd)
+            """), {"id": current_user.id, "data": data_registro, "qtd": quantidade})
         
         total = conn.execute(text("""
-            SELECT agua_consumida 
-            FROM usuarios 
-            WHERE id = :id
-        """), {"id": current_user.id}).scalar()
+            SELECT quantidade_ml 
+            FROM agua_registros 
+            WHERE usuario_id = :id AND data = :data
+        """), {"id": current_user.id, "data": data_registro}).scalar()
         
     return jsonify({
     "mensagem": f"{quantidade}ml registrados com sucesso!",
