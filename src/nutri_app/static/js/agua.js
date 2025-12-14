3cbf15864a76f9881721c26fbbe621593817e7d5
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-12-13 23:41:59 -0300

    Adiciona 'dataSpan' para exibir data selecionada

--- src/nutri_app/static/js/agua.js
+++ src/nutri_app/static/js/agua.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const btn = document.getElementById("btnRegistrarAgua");
     const erro = document.getElementById("erro-agua");
     const totalAgua = document.getElementById("total-agua");
+    const dataSpan = document.getElementById("data-selecionada");
     const seletorData = document.getElementById("seletor-data");
     const btnAnterior = document.getElementById("dia-anterior");
     const btnProximo = document.getElementById("proximo-dia");
