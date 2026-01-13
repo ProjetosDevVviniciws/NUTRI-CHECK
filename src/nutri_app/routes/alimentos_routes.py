50a38d31290c560e9b5bc706fe2e6a6c7481055f
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2026-01-13 14:58:10 -0300

    Remove importação não utilizada do current_user

--- src/nutri_app/routes/alimentos_routes.py
+++ src/nutri_app/routes/alimentos_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from flask_login import login_required, current_user
+from flask_login import login_required
 from sqlalchemy import text
 from src.nutri_app.database import engine
 import requests
