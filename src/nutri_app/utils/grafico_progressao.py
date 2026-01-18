34920fa676344ef35523274a2bce48d39481b75d
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2026-01-17 23:28:47 -0300

    Ajusta gráfico de progressão: renomeia variáveis, aumenta figura e adiciona eixo de datas

--- src/nutri_app/utils/grafico_progressao.py
+++ src/nutri_app/utils/grafico_progressao.py
@@ -1,13 +1,14 @@
 def gerar_grafico_progressao(peso_inicial, datas, pesos):
     import matplotlib.pyplot as plt
     
-    labels = ['Inicial'] + datas
-    valores = [peso_inicial] + pesos if pesos else [peso_inicial]
+    datas_plot = ['Inicial'] + datas
+    pesos_plot = [peso_inicial] + pesos if pesos else [peso_inicial]
 
-    plt.figure(figsize=(5, 3))
-    plt.plot(labels, valores, marker='o')
+    plt.figure(figsize=(6, 3))
+    plt.plot(datas_plot, pesos_plot, marker='o')
     plt.title('Progresso de Peso')
     plt.ylabel('Kg')
+    plt.xlabel('Data')
     plt.grid(True)
 
     plt.tight_layout()
