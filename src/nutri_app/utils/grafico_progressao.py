e7c5c9a0a6e903d6f05cde3be8c9dcf390a48b2f
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2026-01-26 22:13:13 -0300

    Refatora geração do gráfico: imports, conversão de dados e backend Agg

--- src/nutri_app/utils/grafico_progressao.py
+++ src/nutri_app/utils/grafico_progressao.py
@@ -1,6 +1,13 @@
 def gerar_grafico_progressao(datas, pesos):
+    import os
+    import matplotlib
     import matplotlib.pyplot as plt
+    matplotlib.use("Agg")
+    from flask import current_app
 
+    datas = list(datas)
+    pesos = [float(p) for p in pesos]
+    
     plt.figure(figsize=(6, 3))
     plt.plot(datas, pesos, marker='o')
     plt.title('Progresso de Peso')
