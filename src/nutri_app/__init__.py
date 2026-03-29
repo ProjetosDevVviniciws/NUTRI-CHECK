05e19c962403a62d740de8c2645bb9ab3898c201
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2026-03-29 19:58:17 -0300

    Configura envio de emails com Flask-Mail - Inicializa extensão Mail e adiciona configurações via variáveis de ambiente (.env)

--- src/nutri_app/__init__.py
+++ src/nutri_app/__init__.py
@@ -3,15 +3,24 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
 from dotenv import load_dotenv
+from flask_mail import Mail
 
 load_dotenv()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
+mail = Mail()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
+    app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")    
+    app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_DEFAULT_SENDER")
 
+    mail.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = 'login.login'
