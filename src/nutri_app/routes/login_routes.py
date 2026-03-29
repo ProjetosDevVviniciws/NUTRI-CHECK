b918080cdb22fc414c32729fc649a5be319e6a8e
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2026-03-29 18:42:22 -0300

    Simplifica importações dos formulários de senha

--- src/nutri_app/routes/login_routes.py
+++ src/nutri_app/routes/login_routes.py
@@ -3,8 +3,7 @@ from src.nutri_app.database import engine
 from src.nutri_app.utils.hash import verificar_senha
 from src.nutri_app.utils.user_login import UserLogin
 from src.nutri_app.forms.auth_forms import LoginForm
-from src.nutri_app.forms.auth_forms import EsqueciSenhaForm
-from src.nutri_app.forms.auth_forms import ResetarSenhaForm
+from src.nutri_app.forms.auth_forms import EsqueciSenhaForm, ResetarSenhaForm
 from src.nutri_app.utils.token import gerar_token, validar_token
 from src.nutri_app.utils.email_service import enviar_email_reset
 from src.nutri_app.utils.hash import gerar_hash
