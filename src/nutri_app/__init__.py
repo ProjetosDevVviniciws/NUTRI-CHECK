6bd5d17e5e79fe0e54e8b88615723eb82c770e12
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2026-04-05 11:03:49 -0300

    Remove configuração de Flask-Mail e adiciona RESEND_API_KEY

--- src/nutri_app/__init__.py
+++ src/nutri_app/__init__.py
@@ -3,24 +3,16 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
 from dotenv import load_dotenv
-from flask_mail import Mail
 
 load_dotenv()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
-mail = Mail()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-    app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER")
-    app.config['MAIL_PORT'] = int(os.getenv("MAIL_PORT"))
-    app.config['MAIL_USE_TLS'] = os.getenv("MAIL_USE_TLS") == 'True'
-    app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
-    app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")    
-    app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_DEFAULT_SENDER")
-
-    mail.init_app(app)
+    app.config['RESEND_API_KEY'] = os.getenv("RESEND_API_KEY")
+    
     bcrypt.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = 'login.login'
