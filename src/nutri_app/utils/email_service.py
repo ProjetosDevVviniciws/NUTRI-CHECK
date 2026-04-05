db7456c980308febbd88f61318a58fc7463c06b1
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2026-04-05 13:29:40 -0300

    Remove envio assíncrono com threading e uso de anexos CID, adotando envio via API com imagem pública

--- src/nutri_app/utils/email_service.py
+++ src/nutri_app/utils/email_service.py
@@ -1,45 +1,23 @@
-from flask_mail import Message
-from src.nutri_app import mail
 from flask import render_template, current_app
 import os
-import threading
+import resend
 
-def enviar_email_async(app, msg):
-    with app.app_context():
-        try:
-            mail.send(msg)
-        except Exception as e:
-            print("ERRO AO ENVIAR EMAIL:", str(e))
+resend.api_key = os.getenv("RESEND_API_KEY")
 
 def enviar_email_reset(email, link, nome):
-    msg = Message(
-        subject="Recuperação de senha - NutriCheck",
-        recipients=[email]
-    )
     
-    logo_path = os.path.join(current_app.root_path, "static", "images", "logo_email.png")
+    logo_src = "https://nutri-check-nw20.onrender.com/static/images/logo_email.png"
     
-    with open(logo_path, "rb") as fp:
-        msg.attach(
-            filename="logo.png",
-            content_type="image/png",
-            data=fp.read(),
-            disposition="inline",
-            headers={"Content-ID": "<logo>"}
-        )
-
-    logo_src = "cid:logo"
-    
-    msg.html = render_template(
+    html_content = render_template(
         "emails/body_email.html",
         link=link,
         nome=nome,
         logo_src=logo_src
     )
-
-    thread = threading.Thread(
-        target=enviar_email_async,
-        args=(current_app._get_current_object(), msg)
-    )
     
-    thread.start()
+    resend.Emails.send({
+        "from": "NutriCheck <onboarding@resend.dev>",
+        "to": [email],
+        "subject": "Recuperação de senha - NutriCheck",
+        "html": html_content
+    })
